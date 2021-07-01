e9da17e288fde955e931883ae0645dd9bdb2c783
Pranjal290 <56499065+Pranjal290@users.noreply.github.com>
2021-07-01 17:48:39 +0530

    Add files via upload

---
+++ deliver/urls.py
@@ -0,0 +1,36 @@
+"""deliver URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from customer.views import Index, About, Order, OrderConfirmation, OrderPayConfirmation, Menu, Contact, Testimonial
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
+    path('restaurant/', include('restaurant.urls')),
+    path('', Index.as_view(), name='index'),
+    path('about/', About.as_view(), name='about'),
+    path('menu/', Menu.as_view(), name='menu'),
+    path('order/', Order.as_view(), name='order'),
+    path('contact/', Contact.as_view(), name='contact'),
+    path('reviews/', Testimonial.as_view(), name='testimonial'),
+    path('order-confirmation/<int:pk>', OrderConfirmation.as_view(),
+         name='order-confirmation'),
+    path('payment-confirmation/', OrderPayConfirmation.as_view(),
+         name='payment-confirmation'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # adding media URL to access in our app
