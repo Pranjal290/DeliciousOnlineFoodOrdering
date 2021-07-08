dff3e5d51f0abc6d64e70403484616312cca920e
Pranjal290 <56499065+Pranjal290@users.noreply.github.com>
2021-07-08 22:40:41 +0530

    Add files via upload

---
+++ customer/views.py
@@ -0,0 +1,192 @@
+import json
+
+from django.core.mail import send_mail
+from django.db.models import Q
+from django.shortcuts import render, redirect
+from django.views import View
+
+from .models import MenuItem, OrderModel
+
+
+class Index(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/index.html')
+
+
+class About(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/about.html')
+
+
+class Contact(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/contact.html')
+
+class Testimonial(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/testimonial.html')
+
+class Order(View):
+    def get(self, request, *args, **kwargs):
+        # get every item from each category
+        appetizers = MenuItem.objects.filter(category__name__contains='Appetizer')
+        main_courses = MenuItem.objects.filter(category__name__contains='MainCourse')
+        desserts = MenuItem.objects.filter(category__name__contains='Dessert')
+        drinks = MenuItem.objects.filter(category__name__contains='Drink')
+
+        # pass into context
+        context = {
+            'appetizers': appetizers,
+            'main_courses': main_courses,
+            'desserts': desserts,
+            'drinks': drinks,
+        }
+
+        # render the template
+        return render(request, 'customer/order.html', context)
+
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        street = request.POST.get('street')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        zip_code = request.POST.get('zip')
+
+        order_items = {
+            'items': []
+        }
+
+        items = request.POST.getlist('items[]')
+
+        for item in items:
+            menu_item = MenuItem.objects.get(pk=int(item))
+            item_data = {
+                'id': menu_item.pk,
+                'name': menu_item.name,
+                'price': menu_item.price,
+                'category': menu_item.category
+            }
+
+            order_items['items'].append(item_data)
+
+        price = 0
+        item_ids = []
+
+        for item in order_items['items']:
+            price += item['price']
+            item_ids.append(item['id'])
+
+        order = OrderModel.objects.create(
+            price=price,
+            name=name,
+            email=email,
+            street=street,
+            city=city,
+            state=state,
+            zip_code=zip_code
+        )
+        order.items.add(*item_ids)
+
+        # After everything is done, send confirmation email to the user
+        body = ('Thank you for your order! Your food is being made and will be delivered soon!\n'
+                f'Your total: {price}\n'
+                'Thank you again for your order!')
+
+        send_mail(
+            'Thank You For Your Order!',
+            body,
+            'example@example.com',
+            [email],
+            fail_silently=False
+        )
+
+        context = {
+            'items': order_items['items'],
+            'price': price
+        }
+
+        return redirect('order-confirmation', pk=order.pk)
+
+
+class OrderConfirmation(View):
+    def get(self, request, pk, *args, **kwargs):
+        order = OrderModel.objects.get(pk=pk)
+
+        context = {
+            'pk': order.pk,
+            'items': order.items,
+            'price': order.price,
+        }
+
+        return render(request, 'customer/order_confirmation.html', context)
+
+    def post(self, request, pk, *args, **kwargs):
+        data = json.loads(request.body)
+
+        if data['isPaid']:
+            order = OrderModel.objects.get(pk=pk)
+            order.is_paid = True
+            order.save()
+
+        return redirect('payment-confirmation')
+
+
+class OrderPayConfirmation(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'customer/order_pay_confirmation.html')
+
+
+class Menu(View):
+    def get(self, request, *args, **kwargs):
+        appetizers = MenuItem.objects.filter(category__name__contains='Appetizer')
+        main_courses = MenuItem.objects.filter(category__name__contains='MainCourse')
+        desserts = MenuItem.objects.filter(category__name__contains='Dessert')
+        drinks = MenuItem.objects.filter(category__name__contains='Drink')
+
+        # pass into context
+        context = {
+            'appetizers': appetizers,
+            'main_courses': main_courses,
+            'desserts': desserts,
+            'drinks': drinks,
+            'menu_items': appetizers | main_courses | desserts | drinks
+        }
+        return render(request, 'customer/menu.html', context)
+
+class MenuSearch(View):
+    def get(self, request, *args, **kwargs):
+        query = self.request.GET.get("q")
+
+        menu_items = MenuItem.objects.filter(
+            Q(name__icontains=query) |
+            Q(price__icontains=query) |
+            Q(description__icontains=query)
+        )
+
+        context = {
+            'menu_items': menu_items
+        }
+
+        return render(request, 'customer/menu.html', context)
+
+def tracker(request):
+    if request.method == "POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
+        try:
+            order = OrderModel.objects.filter(order_id=orderId, email=email)
+            if len(order) > 0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append({'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps({"status": "success", "updates": updates, "itemsJson": order[0].items_json},
+                                          default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{"status":"no item"}')
+        except Exception as e:
+            return HttpResponse('{"status":"error"}')
+
+    return render(request, 'shop/tracker.html')
